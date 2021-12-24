26df4d4b974f6c03a0262762f737dd6d1bf1d505
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-12-24 18:28:56 +0300

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Dmitry Sharabin on 24.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilitySandbox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AccessibilitySandbox
+//
+//  Created by Dmitry Sharabin on 24.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
